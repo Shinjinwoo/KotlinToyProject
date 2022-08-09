d9f918228d117c5a3ea5f65048f7afc73dcf3ebe
Shinjinwoo <jinu712@gmail.com>
2022-08-09 20:45:47 +0900

    Grid RecyclerView

--- app/src/main/java/com/example/toyproject/PhotoCollectionActivity.kt
+++ app/src/main/java/com/example/toyproject/PhotoCollectionActivity.kt
@@ -1,12 +1,25 @@
 package com.example.toyproject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.toyproject.model.Photo
+import com.example.toyproject.utils.Constants.TAG
 
 class PhotoCollectionActivity :AppCompatActivity(){
 
+    var photoList = ArrayList<Photo>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var bundle = intent.getBundleExtra("photo_array_list")
+        var searchTerm = intent.getStringExtra("search_term")
+
+        photoList = bundle?.getSerializable("array_bundle") as ArrayList<Photo>
+
+        Log.d(TAG,"PhotoCollectionActivity - onCreate Called :::: searchTerm : $searchTerm, photoArrayList : ${photoList.count()}")
+
 
 
         setContentView(R.layout.activity_photo_collection)
