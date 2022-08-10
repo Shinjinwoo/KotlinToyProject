dc94282295ff6b72afe50441fa5d3d58a8a40294
Shinjinwoo <jinu712@gmail.com>
2022-08-11 00:04:55 +0900

    Grid RecyclerView 디자인

---
+++ app/src/main/java/com/example/toyproject/recyclerview/PhotoGridRecyclerViewAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.toyproject.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoGridRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+
+}
